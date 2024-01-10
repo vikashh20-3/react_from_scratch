be199899840cd9706b391f88358d9ee519ddc634
vikash <vvikashrao203@gmail.com>
2024-01-10 11:14:37 +0530

    useState hook added

--- src/content.js
+++ src/content.js
@@ -1,9 +1,13 @@
+ import { useState } from "react";
+
 const Content = () => {
+    const  [name , SetName]=useState('vikash');
 
     const handleNameChange = () => {
         const names = ['vikash', 'viku', 'vikku'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        // return names[int];
+        SetName(names[int]);
     }
 
     const handlebuttoclickevent = () => {
@@ -13,10 +17,21 @@ const Content = () => {
     const handleclickeventsecond = (name) => {
         return console.log(`${name} is clicked`)
     };
+
+    const handleclickeventseconde = (e) => {
+        return console.log(e)
+    };
+    // this e will show the properties of this onclick 
+    // we can furthur more explore this like 
+    // e.target
+    // this will show on which xml tag it should be used
+    // e.target.innertext
+    // this will show that what is the innter text of that tag
     return (
         <main>
             <p>
-                Hello {handleNameChange()}!
+                {/* Hello {handleNameChange()}! */}
+                Hello {name};
             </p>
             {/* we can render js code as a string 
 
@@ -29,7 +44,8 @@ const Content = () => {
             {/* it will show that object cannot be rendered */}
             {/* boolean can also be not rendered in this way */}
 
-            <button onClick={handlebuttoclickevent}>You Clicked this upper button</button>
+            {/* <button onClick={handlebuttoclickevent}>You Clicked this upper button</button> */}
+            <button onClick={handleNameChange}>To change the name with useState</button>
             <button onClick={() => handleclickeventsecond('vikash')}>You Clicked this parameter  button</button>
 
         </main>
